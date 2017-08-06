b3e76fab2af4fe378d8392020e836e6570e4e053
ThomasTrzos <trzos.tomasz@gmail.com>
2017-08-06 20:11:58 +0200

    Added AI: computer will win in a situation that user won't block any computer moves and in a situation that computer has first move and user hadn't choose middle field in his first move.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,31 @@
 
-var userA = "";
-var userB = ""; // or computer
+let userA = "";
+let userB = ""; // or computer
 
-var gameMode;
-var gameChoice;
-var gameBoard;
-var gameResult;
+let gameMode;
+let gameChoice;
+let gameBoard;
+let gameResult;
 
-var combos;
-var board;
-var movesCounter = 0;
+let board;
+let movesCounter = 0;
 
-var gameWithComputer; // true - computer, false - two players
-var turn = true; // whose turn is now (true - playerOne, false - playerTwo)
+let gameWithComputer; // true - computer, false - two players
+let computerStarted;
+let turn = true; // whose turn is now (true - playerOne, false - playerTwo)
+
+const corners = [1, 3, 7, 9];
+
+const combos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 
 
@@ -27,19 +40,17 @@ window.onload = function(){
 
     // -----------------
 
-    var fields = document.getElementById("fields");
-    var li = fields.getElementsByTagName("li");
+    let fields = document.getElementById("fields");
+    let li = fields.getElementsByTagName("li");
 
-    for (var i=0; i<li.length; i++)
+    for (let i=0; i<li.length; i++)
     {
         li[i].addEventListener("click", function(e) {
 
             if(e.target.innerHTML === "...") {
 
-                if(turn) {
-                    e.target.innerHTML = userA;
-                    e.target.style.color = "rgba(220, 220, 220, .7)";
-                    htmlListToArray();
+                if(turn) { // player one
+                    makeMove(e, userA);
 
                     if(checkGameResult(userA) && movesCounter <= 9) {
                         if(!gameWithComputer) {
@@ -54,12 +65,78 @@ window.onload = function(){
                     }
 
                     turn = false;
-                } else {
-                    e.target.innerHTML = userB;
-                    e.target.style.color = "rgba(220, 220, 220, .7)";
-                    htmlListToArray();
+                } else { // player two or computer
 
-                    if(checkGameResult(userB && movesCounter <= 9)) {
+                    if(!gameWithComputer) { // player two
+                        makeMove(e, userB);
+                    } else { // computer moves
+
+                        if(computerStarted) {
+
+                            if(movesCounter === 0 ) { // should choose any corner
+                                computerMakesMove(document.getElementById("1"), userB);
+                            }
+
+                            if(movesCounter === 2) {
+                                if(checkIsFieldEmpty(5)) { // middle field has not chosen, win situation
+                                    for(let element of corners) {
+                                        if(checkIsFieldEmpty(element)) {
+                                            computerMakesMove(document.getElementById(element.toString()), userB);
+                                            break;
+                                        }
+                                    }
+                                } else {
+                                    // what then?
+                                }
+                            }
+
+                            if(movesCounter === 4) {
+                                let move = findMoveToWin(userB);
+                                if(move !== -1) {
+                                    computerMakesMove(document.getElementById(move.toString()), userB);
+                                } else {
+                                    for(let element of corners) {
+                                        if(checkIsFieldEmpty(element)) {
+                                            computerMakesMove(document.getElementById(element.toString()), userB);
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+
+                            if(movesCounter === 6) {
+                                let move = findMoveToWin(userB);
+                                if(move !== -1) {
+                                    computerMakesMove(document.getElementById(move.toString()), userB);
+                                }
+                            }
+
+
+
+
+
+
+
+                        } else {
+
+                            if(movesCounter === 2) { // should choose a middle field
+                                computerMakesMove(document.getElementById("5"), userB);
+                            }
+
+                        }
+
+
+
+
+
+
+
+
+
+
+                    }
+
+                    if(checkGameResult(userB) && movesCounter <= 9) {
                         if(!gameWithComputer) {
                             showGameResults("Player 2 wins!");
                         } else {
@@ -79,12 +156,12 @@ window.onload = function(){
     }
 
     addListenerForElement("one-btn", function action() {
-        gameWithComputer = false;
+        gameWithComputer = true;
         addAnimBetweenDivs(gameMode, gameChoice);
     });
 
     addListenerForElement("two-btn", function action() {
-        gameWithComputer = true;
+        gameWithComputer = false;
         addAnimBetweenDivs(gameMode, gameChoice);
     });
 
@@ -111,9 +188,25 @@ window.onload = function(){
     })
 };
 
+function makeMove(element, user) {
+    element.target.innerHTML = user;
+    element.target.style.color = "rgba(220, 220, 220, .7)";
+    htmlListToArray();
+}
+
+function computerMakesMove(element, computer) {
+    element.innerHTML = computer;
+    element.style.color = "rgba(220, 220, 220, .7)";
+    htmlListToArray();
+}
+
+function checkIsFieldEmpty(fieldNum) {
+    return board[fieldNum - 1] === "...";
+}
+
 function addListenerForElement(id, action) {
 
-    var element = $("#" + id);
+    let element = $("#" + id);
 
     element.on("click", function() {
         action();
@@ -178,18 +271,7 @@ function checkGameResult(symbol) {
 
     movesCounter+=1;
 
-    combos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-
-    for(var i = 0; i<combos.length; i++) { // it could be checked using jquerry without translate html array to js
+    for(let i = 0; i<combos.length; i++) { // it could be checked using jquerry without translate html array to js
 
         if(board[combos[i][0]] === symbol && board[combos[i][1]] === symbol && board[combos[i][2]] === symbol) {
             return true;
@@ -199,14 +281,48 @@ function checkGameResult(symbol) {
     return false;
 }
 
+function findMoveToWin(symbol) {
+
+    for(let element of combos) {
+
+        console.log("DEBUG: " + board[element[0]] + " " + board[element[1]] + " " + board[element[2]]);
+
+        if(board[element[0]] === "..." && board[element[1]] === symbol && board[element[2]] === symbol) {
+            console.log(board[element[0]] + " " + userA + " COMPARISION ");
+            if(board[element[0]] !== userA) {
+                return element[0] + 1;
+            }
+
+        }
+
+        if(board[element[0]] === symbol && board[element[1]] === "..." && board[element[2]] === symbol) {
+            console.log(board[element[1]] + " " + userA + " COMPARISION ");
+            if(board[element[1]] !== userA) {
+                return element[1] + 1;
+            }
+        }
+
+        if(board[element[0]] === symbol && board[element[1]] === symbol && board[element[2]] === "...") {
+            console.log(board[element[2]] + " " + userA + " COMPARISION ");
+            if(board[element[2]] !== userA) {
+                return element[2] + 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
 function checkWhoStars() {
 
-    turn = Math.floor((Math.random() * 10) + 1) % 2 === 0; // true - playerOne, false - playerTwo
+    //turn = Math.floor((Math.random() * 10) + 1) % 2 === 0; // true - playerOne, false - playerTwo
+    turn = false;
 
     if(turn) {
-        alert("Your turn!"); // TODO: innerHTML
+        //alert("Your turn!"); // TODO: innerHTML
     } else {
-        alert("UserB turn"); // TODO: innerHTML
+        //alert("UserB turn"); // TODO: innerHTML
+        computerStarted = true;
     }
 
 }
